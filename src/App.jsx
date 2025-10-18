c82b7557a62df9624d2042f164f4b8a9f211067b
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2025-10-18 19:19:06 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from "./components/nav/nav"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <nav></nav>
+    </>
+  )
+}
+
+export default Nav
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./nav.css"
+function nav() {
+  return (
+    <nav>
+        <h1>PORTFOLIO</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default nav
